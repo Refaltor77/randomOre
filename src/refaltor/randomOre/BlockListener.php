e71c00fa3eec2247a7ed5336db04d82d21277362
morcheysha77 <44336443+morcheysha77@users.noreply.github.com>
2021-08-05 11:07:17 +0200

    Update BlockListener.php

--- src/refaltor/randomOre/BlockListener.php
+++ src/refaltor/randomOre/BlockListener.php
@@ -22,24 +22,31 @@ class BlockListener implements Listener
         $array = $this->plugin->getConfig()->getAll();
 
         if ($block->getId() . ":" . $block->getDamage() === $array['randomOre']){
+            
+            $drops = [];
+            
             foreach ($array['drops'] as $id => $keys){
                 
                 $item = explode(":", $id);
+                $item = Item::get($item[0], $item[1], $keys['amount']);
                 
-                if (isset($keys['chance'])){
-                    if (mt_rand(1, $keys['chance']) === 1){               
+                if(isset($keys['name']) AND $keys['name'] !== null)
+                    $item->setCustomName($keys['name']);                
+                if (empty($keys['chance']) OR mt_rand(1, 100) === intval($keys['chance'])){               
                         $item = Item::get($item[0], $item[1], $keys['amount']);
                         if (isset($keys['name']) && $keys['name'] !== null) 
                             $item->setCustomName($keys['name']);
-                        $event->setDrops([$item]);
+    $drops[] = $item;
                     }
                 } else {
                    $item = Item::get($item[0], $item[1], $keys['amount']);
                    if (isset($array['name']) && $keys['name'] !== null) 
                        $item->setCustomName($keys['name']);
-                   $event->setDrops([$item]);
+                   $drops[] = $item;
                 }
             }
+            
+            $event->setDrops($drops);
         }
     }
 }
